f83f12995f1aaaac6e67e21a7ad012389ec21e8b
THEBR4Y4N <garzonbrayan1201@hotmail.com>
2022-03-11 15:19:01 -0500

    Fase 2

---
+++ MiIngresoHitss/Controllers/ListProdController.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MiIngresoHitss.Context;
+using MiIngresoHitss.Models;
+
+namespace MiIngresoHitss.Controllers
+{
+    public class ListProdController : Controller
+    {
+        private readonly MVCcontext _context;
+
+        public ListProdController(MVCcontext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products1
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Productos.ToListAsync());
+        }
+    }
+}
